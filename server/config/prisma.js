4e3a29662eff1b058d5f26cfc566105477f8d4ed
Kunal Sharma <68844042+Roshians@users.noreply.github.com>
2026-04-29 02:04:46 +0530

    Downgrade Prisma from 7.x to 5.x for Render deployment compatibility

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -1,15 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
 const logger = require('../utils/logger');
-require('dotenv').config();
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
-  adapter,
   log: [
     { emit: 'event', level: 'query' },
     { emit: 'stdout', level: 'error' },
